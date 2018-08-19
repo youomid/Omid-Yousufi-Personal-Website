b2eea9e3850cf6d51af5bd258346b7f0d2ffe6db
Omid Yousufi <omid_yousufi@hotmail.com>
2018-08-19 12:23:20 -0400

    Created structure for introduction and main component and added basic details

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -3,9 +3,13 @@ import React from 'react';
 export default class Introduction extends React.Component {
   render() {
     return (
-      <div>
-        Introduction
+      <div id="intro">
+        <div >
+          <img height="250" width="200" src="/img/profile.jpg" alt="" />
+        </div>
+        <h1>Omid Yousufi</h1>
+        <p>Software Engineer -  Toronto, Ontario</p>
       </div>
-    );
+  );
   }
 }
