5d31c18c2a71f4a91c433580b15c9969936e7e7a
Omid Yousufi <omid_yousufi@hotmail.com>
2018-08-19 10:41:11 -0400

    Created background canvas

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -3,11 +3,33 @@ import React from 'react';
 export default class IndexPage extends React.Component {
   render() {
     return (
-      <div className="home">
-        <div>
-          IndexPage
-        </div>
+      <div >
+      	<canvas id="bg-canvas">Error, canvas is not supported</canvas>
       </div>
     );
   }
+
+  componentDidMount() {
+  	var canvas = document.getElementById('bg-canvas');
+	var context = canvas.getContext('2d');
+
+	// dynamically set height and width
+	window.addEventListener('resize', resizeCanvas, false);
+
+    function resizeCanvas() {
+    	console.log('Resizing canvas');
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        drawBackground()
+    }
+
+    resizeCanvas()
+
+    function drawBackground() {
+		context.fillStyle = "blue";
+		context.fillRect(0, 0, canvas.width, canvas.height);
+    }
+
+  }
+
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,19 +4,7 @@ import { Link } from 'react-router';
 export default class Layout extends React.Component {
   render() {
     return (
-      <div>
-        <header>
-          <Link to="/">
-            <img height="250" width="200" src="/img/profile.jpg"/>
-          </Link>
-        </header>
-        <div>{this.props.children}</div>
-        <footer>
-          <p>
-            This is Omid Yousufi's personal website using <strong>React</strong> and <strong>Express</strong>.
-          </p>
-        </footer>
-      </div>
+      <div>{this.props.children}</div>
     );
   }
 }
