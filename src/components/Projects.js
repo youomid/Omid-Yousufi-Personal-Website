bd382ecc6afe5406a2e56a16016aee87e5f5090b
Omid Yousufi <omid_yousufi@hotmail.com>
2018-09-19 21:25:07 -0400

    Changed project section header design

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,10 +9,11 @@ export default class Projects extends React.Component {
 			</header>
 			<div className="row projects">
 				<article className="project col-lg-6">
-					<a href="https://github.com/youomid/MeasureMe" className="image fit"><img src="img/measureme.png" alt="" /></a>
-					<header>
-						<h2><a href="https://github.com/youomid/MeasureMe">MeasureMe</a><span className="floatRight">05/2018 - Present</span></h2>
+					<header style={{textAlign: "center"}}>
+						<p>05/2018 - Present</p>
+						<h2><a href="https://github.com/youomid/MeasureMe"></a>MeasureMe</h2>
 					</header>
+					<a href="https://github.com/youomid/MeasureMe" className="image fit"><img src="img/measureme.png" alt="" /></a>
 					<div className="projectDescription">
 						<p>A personal analytics web application that uses a realtime distributed event processing system to provide insight into a user's productivity.</p>
 					</div>
@@ -23,10 +24,11 @@ export default class Projects extends React.Component {
 					</div>
 				</article>
 				<article className="project col-lg-6">
-					<a href="spritegen/" className="image fit"><img src="img/sprite_gen.png" alt="" /></a>
-					<header>
-						<h2><a style={{float:"left"}} href="spritegen/">SpriteGen</a><span className="floatRight">02/2018 - 04/2018</span></h2>
+					<header style={{textAlign: "center"}}>
+						<p>02/2018 - 04/2018</p>
+						<h2><a href="spritegen/"></a>SpriteGen</h2>
 					</header>
+					<a href="spritegen/" className="image fit"><img src="img/sprite_gen.png" alt="" /></a>
 					<div className="projectDescription">
 						<p>A web application that procedurally generates 2D pixel sprites using the Pixel Robot algorithm.</p>
 					</div>
@@ -36,10 +38,11 @@ export default class Projects extends React.Component {
 					</div>
 				</article>
 				<article className="project col-lg-6">
-					<a href="tiltorcarry/" className="image fit"><img src="img/toc_home.png" alt="" /></a>
-					<header>
-						<h2><a href="tiltorcarry/">TiltOrCarry</a><span className="floatRight">01/2017 - 01/2018</span></h2>
+					<header style={{textAlign: "center"}}>
+						<p>01/2017 - 01/2018</p>
+						<h2><a href="tiltorcarry/"></a>TiltOrCarry</h2>
 					</header>
+					<a href="tiltorcarry/" className="image fit"><img src="img/toc_home.png" alt="" /></a>
 					<div className="projectDescription">
 						<p>A data analytics web application for League Of Legends that uses statistics and machine learning to analyze data.</p>
 					</div>
@@ -52,10 +55,11 @@ export default class Projects extends React.Component {
 					</div>
 				</article>
 				<article className="project col-lg-6">
-					<a href="https://play.google.com/store/apps/details?id=com.voltropic.imdo.dynamictimer" className="image fit"><img src="img/dynamic_timer.png" alt="" /></a>
-					<header>
-						<h2><a href="https://play.google.com/store/apps/details?id=com.voltropic.imdo.dynamictimer">Dynamic Timer</a><span className="floatRight">09/2015 - 02/2016</span></h2>
+					<header style={{textAlign: "center"}}>
+						<p>09/2015 - 02/2016</p>
+						<h2><a href="https://play.google.com/store/apps/details?id=com.voltropic.imdo.dynamictimer"></a>Dynamic Timer</h2>
 					</header>
+					<a href="https://play.google.com/store/apps/details?id=com.voltropic.imdo.dynamictimer" className="image fit"><img src="img/dynamic_timer.png" alt="" /></a>
 					<div className="projectDescription">
 						<p>An android mobile application that helps improve productivity through the break point system.</p>
 					</div>
