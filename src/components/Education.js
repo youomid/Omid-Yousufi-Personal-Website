a0022801bd67820cd5d116c6526be2a3a6580f9c
Omid Yousufi <omid_yousufi@hotmail.com>
2018-09-22 12:54:14 -0400

    Cleaned up education section

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,21 +1,49 @@
 import React from 'react';
 
 export default class Education extends React.Component {
-  render() {
-    return (
-		<section style={{borderBottom: "solid 2px #eeeeee"}}>
-			<header className="sectionHeader">
-				Education
-			</header>
-			<div className="education">
-				<img className="schoolLogo" src="img/mcmaster_logo.png" />
-				<header>
-					<h2>Bachelor of Engineering, Electrical </h2>
-					<p style={{marginBottom: ".25rem"}}>McMaster University - Hamilton, Ontario<span className="floatRight">2011 - 2015</span></p>
-					<p style={{marginBottom: ".5rem"}}><strong>Courses: </strong>Analog Circuits, Power Systems, etc.</p>
+
+	constructor(props){
+		super(props);
+		this.education = [
+			{
+				degree: "Bachelor of Engineering, Electrical",
+				date: "2011 - 2015",
+				logo: "img/mcmaster_logo.png",
+				address: "McMaster University - Hamilton, Ontario",
+				courses: "Analog Circuits, Power Systems, etc."
+			}
+		];
+	}
+
+	generateEducationHtml() {
+		var education = [];
+
+		for(var i =0; i < this.education.length; i++) {
+			education.push(
+				<div className="education">
+					<img className="schoolLogo" src={this.education[i].logo} />
+					<header>
+						<h2>{this.education[i].degree}</h2>
+						<p style={{marginBottom: ".25rem"}}>{this.education[i].address}<span style={{float:"right"}}>{this.education[i].date}</span></p>
+						<p style={{marginBottom: ".5rem"}}><strong>Courses: </strong>{this.education[i].courses}</p>
+					</header>
+				</div>
+			);
+		}
+
+		return education;
+	}
+
+	render() {
+		return (
+			<section style={{borderBottom: "solid 2px #eeeeee"}}>
+				<header className="sectionHeader">
+					Education
 				</header>
-			</div>
-		</section>
-);
-  }
+				{
+					this.generateEducationHtml()
+				}
+			</section>
+		);
+	}
 }
