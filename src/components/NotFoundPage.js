c916d9304620dab935ca072cf9274d64a3b70e7b
Omid Yousufi <omid_yousufi@hotmail.com>
2018-09-23 10:23:35 -0400

    Update 404 page not found page and added profile pic

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -7,9 +7,6 @@ export default class NotFoundPage extends React.Component {
       <div className="not-found">
         <h1>404</h1>
         <h2>Page not found!</h2>
-        <p>
-          <Link to="/">Go back to the main page</Link>
-        </p>
       </div>
     );
   }
