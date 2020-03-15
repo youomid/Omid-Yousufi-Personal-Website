dd9f4359ab940078fc3a8ca563176ad168b30eb4
Omid Yousufi <omid_yousufi@hotmail.com>
2020-03-15 15:56:27 -0400

    More link updates and also update bundle.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,7 @@ export default class Projects extends React.Component {
 				name: "StyleToClass",
 				date: "07/2019 - 08/2019",
 				link: "https://github.com/youomid/StyleToClass",
-				image: "img/styletoclass.gif",
+				image: "src/static/img/styletoclass.gif",
 				description: "A sublime text plugin that converts all the style attributes in an html file to classes.",
 				technologies: ["Python", "BeautifulSoup"]
 			},
@@ -17,7 +17,7 @@ export default class Projects extends React.Component {
 				name: "MeasureMe",
 				date: "05/2018 - 01/2019",
 				link: "https://github.com/youomid/MeasureMe",
-				image: "img/measureme.png",
+				image: "src/static/img/measureme.png",
 				description: "A personal analytics web application that uses a realtime distributed event processing system to provide insight into a user's productivity.",
 				technologies: ["Docker", "Angular6", "Django", "NodeJS"]
 			},
@@ -25,7 +25,7 @@ export default class Projects extends React.Component {
 				name: "SpriteGen",
 				date: "02/2018 - 04/2018",
 				link: "https://github.com/youomid/SpriteGen/",
-				image: "img/sprite_gen.png",
+				image: "src/static/img/sprite_gen.png",
 				description: "A web application that procedurally generates 2D pixel sprites using the Pixel Robot algorithm.",
 				technologies: ["AngularJS", "ExpressJS"]
 			},
@@ -33,7 +33,7 @@ export default class Projects extends React.Component {
 				name: "TiltOrCarry",
 				date: "01/2017 - 01/2018",
 				link: "https://github.com/youomid/TiltOrCarry/",
-				image: "img/toc_home.png",
+				image: "src/static/img/toc_home.png",
 				description: "A data analytics web application for League Of Legends that uses statistics and machine learning to analyze data.",
 				technologies: ["React", "Redux", "Django", "Celery", "PostgreSQL", "Logistic Regression"]
 			},
@@ -41,7 +41,7 @@ export default class Projects extends React.Component {
 				name: "Dynamic Timer",
 				date: "09/2015 - 02/2016",
 				link: "https://play.google.com/store/apps/details?id=com.voltropic.imdo.dynamictimer",
-				image: "img/dynamic_timer.png",
+				image: "src/static/img/dynamic_timer.png",
 				description: "An android mobile application that helps improve productivity through the break point system.",
 				technologies: ["Java", "XML", "SQL", "Android Studio"]
 			},
